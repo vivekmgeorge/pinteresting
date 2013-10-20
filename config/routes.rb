07febca32e268d8f2c398ab9e74cb24540c362ae
Vivek George <vivek.m.george@gmail.com>
2013-10-20 01:52:37 -0400

    Added another pages a few more customizations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" # creates about_path
-  
+  get "blog" => "pages#blog" # creates blog_path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
