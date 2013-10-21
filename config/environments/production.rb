f9639ebe7c9a85ea97ea7daab94e430a224501af
Vivek George <vivek.m.george@gmail.com>
2013-10-20 20:15:27 -0400

    Implmented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # required for heroku
+  # note to set this to your actual host
+  config.action_mailer.default_url_options = { :host => 'viveksandbox.com' }
 end
